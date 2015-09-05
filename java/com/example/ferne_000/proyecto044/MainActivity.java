17f8924c512642fd2db586ca644df21cb9e75373
Jth Rdz <fernetsystem@gmail.com>
2015-09-05 06:46:36 -0500

    quitar Barra de app y cambiar compilacion Android 15

--- java/com/example/ferne_000/proyecto044/MainActivity.java
+++ java/com/example/ferne_000/proyecto044/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.ferne_000.proyecto044;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 
